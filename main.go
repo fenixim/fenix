8eac998b7abbdbda3d4993c94328b8caee6bbbc3
piesquared <40523150+piecubed@users.noreply.github.com>
2022-07-29 10:32:55 -0400

    Remove unneeded comment

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 )
 
 func main() {
-	// addr := flag.String("addr", ":8080", "http service address")
-	// flag.Parse()
 	wg := utils.WaitGroupCounter{}
 	hub := server.NewHub(&wg)
 	server.Serve("0.0.0.0:8080", &wg, hub)
