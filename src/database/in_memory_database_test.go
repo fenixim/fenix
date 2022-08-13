27450bc65390433dcfbd23b069f526a4cb1e11cc
JosiahWI <josiah_vanderzee@mediacombb.net>
2022-08-13 11:00:30 -0500

    add test for message author

--- src/database/in_memory_database_test.go
+++ src/database/in_memory_database_test.go
@@ -7,7 +7,12 @@ import (
 	"time"
 )
 
-func TestInMemoryDatabase(t *testing.T) {
+type testCase struct {
+	author  string
+	content string
+}
+
+func TestBasicOperations(t *testing.T) {
 	t.Run("empty message history", func(testing *testing.T) {
 		db := database.NewInMemoryDatabase()
 
@@ -34,23 +39,38 @@ func TestInMemoryDatabase(t *testing.T) {
 					database.NewMessage("gopher", "hello"))
 			}
 
-			got := len(db.GetMessagesBetween(0, time.Now().Unix(), 50))
+			got := len(db.GetMessagesBetween(
+				0, time.Now().Unix(), 50))
 			expected := i
 			test_utils.AssertEqual(t, got, expected)
 		}
 	})
 
-	t.Run("message content", func(testing *testing.T) {
-		testCases := []string{"hello", "yay"}
+}
 
-		for _, test := range(testCases) {
-			db := database.NewInMemoryDatabase()
-			db.InsertMessage(database.NewMessage("gopher", test))
+func TestMessages(t *testing.T) {
+	testCases := []testCase{
+		{"gopher", "hello"},
+		{"bloblet", "yay"},
+	}
 
-			history := db.GetMessagesBetween(0, time.Now().Unix(), 1)
+	for _, test := range testCases {
+		db := database.NewInMemoryDatabase()
+		db.InsertMessage(
+			database.NewMessage(test.author, test.content))
+
+		history := db.GetMessagesBetween(0, time.Now().Unix(), 1)
+
+		t.Run("message content", func(testing *testing.T) {
 			got := history[0].Content
-			expected := test
+			expected := test.content
 			test_utils.AssertEqual(t, got, expected)
-		}
-	})
+		})
+
+		t.Run("message author", func(testing *testing.T) {
+			got := history[0].Author
+			expected := test.author
+			test_utils.AssertEqual(t, got, expected)
+		})
+	}
 }
