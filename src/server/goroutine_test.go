4d4be8c47363aa8b964ae3d2396fec7f0a6f752e
piesquared <40523150+piecubed@users.noreply.github.com>
2022-08-14 13:29:37 -0400

    remove unneeded serverstop test

--- src/server/goroutine_test.go
+++ src/server/goroutine_test.go
@@ -25,32 +25,4 @@ func TestEnsureGoroutinesStop(t *testing.T) {
 			t.Fail()
 		}
 	})
-	t.Run("when server exits after client connected", func(t *testing.T) {
-
-		srv := test_utils.StartServer()
-		srv.Addr.Path = "/register"
-		cli := test_utils.Connect("gopher123", "totallymypassword", srv.Addr)
-		cli.Close()
-		srv.Close()
-		time.Sleep(5 * time.Second)
-
-		srv.Wg.Wait()
-		got := srv.Wg.Counter
-		expected := 0
-		if got != expected {
-			t.Fail()
-		}
-	})
-	t.Run("when server exits", func(t *testing.T) {
-
-		srv := test_utils.StartServer()
-		srv.Close()
-
-		srv.Wg.Wait()
-		got := srv.Wg.Counter
-		expected := 0
-		if got != expected {
-			t.Fail()
-		}
-	})
 }
