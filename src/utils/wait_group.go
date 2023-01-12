472b135a86f0e16ea620ce333ae399d4b11faabc
piesquared <40523150+piecubed@users.noreply.github.com>
2023-01-11 22:04:17 -0500

    Fix atomic issue with waitgroups

--- src/utils/wait_group.go
+++ src/utils/wait_group.go
@@ -1,37 +1,39 @@
 package utils
 
 import "sync"
+import "sync/atomic"
 
 // Custom sync.WaitGroup equivalent, for testing purposes
 // Has counter visible for easy monitoring of goroutines and Names for what goroutine is still running
 // To create WaitGroupCounter, call NewWaitGroupCounter()
 type WaitGroupCounter struct {
 	WaitGroup *sync.WaitGroup
-	Counter   int
+	Counter   int64
 	Names     *sync.Map
 }
 
 // Adds a goroutine to the WaitGroup
 // delta cannot be negative
 // name must be unique
-func (w *WaitGroupCounter) Add(delta int, name string) error {
+func (w *WaitGroupCounter) Add(delta int64, name string) error {
 	if w.Names == nil {
 		panic("wg: Names field uninitialized")
 	}
 	if _, ok := w.Names.Load(name); ok {
-		return nil // not correct logic so could be related
+		return nil 
 	}
-	w.Counter += delta
+	atomic.StoreInt64(&w.Counter, atomic.AddInt64(&w.Counter, delta))
 
 	w.Names.Store(name, true)
-	w.WaitGroup.Add(delta)
+	w.WaitGroup.Add(int(delta))
 	return nil
 }
 
 // Removes a goroutine from the waitgroup
 // name must be the same name provided to Add
 func (w *WaitGroupCounter) Done(name string) {
-	w.Counter--
+	atomic.StoreInt64(&w.Counter, atomic.AddInt64(&w.Counter, -1))
+	
 	w.Names.Delete(name)
 
 	w.WaitGroup.Done()
